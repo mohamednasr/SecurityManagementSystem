5b48d0dd3573ed490ecf52691d79c85c0213c61d
mohamednasr <mohamed.nasr.shiref@gmail.com>
2023-03-03 05:24:16 +0300

    temp push

---
+++ SecurityMS.Core.Models/SiteRewardsModel.cs
@@ -0,0 +1,26 @@
+﻿using DataAnnotationsExtensions;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SecurityMS.Core.Models
+{
+    public class SiteRewardsModel
+    {
+        [Display(Name = "الموقع")]
+        [Required]
+        [Min(1)]
+        public long SiteId { get; set; }
+
+        [Display(Name = "تاريخ الحافز")]
+        public DateTime RewardDate { get; set; } = DateTime.Now;
+
+        [Display(Name = "نوع الحافز")]
+        public int RewardType { get; set; }
+        [Display(Name = "القيمه / الأيام")]
+        [Required]
+        [Min(1)] 
+        public decimal Amount { get; set; }
+        [Display(Name = "السبب")]
+        public string Reason { get; set; }
+    }
+}
